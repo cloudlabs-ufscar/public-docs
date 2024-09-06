931d17fb7641af65022efd3f3ac007d27b9c0620
Daniel Lombardi <lombardi.daniel.o@gmail.com>
2024-09-06 19:56:11 -0300

    workflows

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -8,7 +8,8 @@ const config: Config = {
   favicon: "img/favicon.ico",
 
   // Set the production url of your site here
-  url: "https://cloudlabs.ufscar.br",
+  // url: "https://cloudlabs.ufscar.br",
+  url: "https://pesquisa-cloud-ufscar.github.io/",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: "/",
